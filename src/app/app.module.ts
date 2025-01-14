f24ca9a965780e2e2a395ed2bf20d267bdcdb329
GAuta-hub <gautamgc95@gmail.com>
2025-01-14 15:20:51 +0530

    init commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,50 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatOptionModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import {MatMenuModule} from '@angular/material/menu';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { LoginComponent } from './login/login.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+   
+  ],
+  imports: [
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDividerModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatMenuModule,
+    CommonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+  ],
+  providers: [
+    provideClientHydration(withEventReplay()),
+    provideAnimationsAsync()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit, signal } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-login',
+  standalone: false,
+  
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent implements OnInit{
+
+  loginForm!: FormGroup;
+  hide = signal(true);
+  alertMessage: any;
+  showModal = false; //show model is control flag that control the model is visible or not
+
+  constructor(private fb: FormBuilder, private router: Router) {
+
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.maxLength(10), Validators.required]],
+      password: ['', [Validators.required]]
+    })
+  }
+
+  toggleModal() { //method toggles the visibility of the modal
+    this.showModal = !this.showModal;
+  }
+  
+  
+  
+  
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+
+  onSignup() {
+    this.router.navigate(["/signup"])
+    }
+    onForgot() {
+    throw new Error('Method not implemented.');
+    }
+    clickEvent($event: MouseEvent) {
+    throw new Error('Method not implemented.');
+    }
+    onSubmit() {
+    this.router.navigate(["/dashboard"])
+    }
+      
+}
+
+
+
+
+
